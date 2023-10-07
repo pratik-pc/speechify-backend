40fadd9b7caf0834dc2ec548b59841f284d3fb53
Pratik <pratikchanda2000@gmail.com>
2023-10-07 18:27:18 +0530

    feat: add config and environment variables

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from src.routes import Transcription
+from src.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 app.add_url_rule('/api/transcribe', view_func=Transcription.as_view('transcribe'))
---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:   # Config for the app instance
+
+  AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+  AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+  AWS_REGION = os.getenv('AWS_REGION')
+  S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
