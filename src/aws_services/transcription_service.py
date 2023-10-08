6fe27576f587dad5abaa26afa07b501cd7f5e901
Pratik <pratikchanda2000@gmail.com>
2023-10-08 14:53:22 +0530

    feat: add post request to send audio file

---
+++ src/aws_services/transcription_service.py
@@ -0,0 +1,3 @@
+def transcribe(audio):
+  print(audio)
+  return "hello"
--- src/routes/transcribe_routes.py
+++ src/routes/transcribe_routes.py
@@ -1,5 +1,11 @@
+from flask import request
 from flask.views import MethodView
+from src.aws_services.transcription_service import transcribe
 
 class Transcription(MethodView):
-  def get(self):
-    return "Transcription route"
+  def post(self):
+    audio_file = request.files.get('audio')
+    if not audio_file:
+      return "Error Audio file is missing"
+    if audio_file:
+      return transcribe(audio=audio_file)
